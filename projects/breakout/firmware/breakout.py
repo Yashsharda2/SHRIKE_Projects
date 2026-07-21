f625933930b445e663ae1afe5606fdaad305070e
Yashsharda2 <yashsharda2006@gmail.com>
2026-07-21 20:13:33 +0530

    update firmware

--- projects/breakout/firmware/breakout.py
+++ projects/breakout/firmware/breakout.py
@@ -1,11 +1,6 @@
-# breakout.py
-# Author: Yash Sharda
-# Interactive controller for the WS2812 Breakout game on Shrike Lite.
 
 import time
 import shrike
 from machine import Pin, SPI
 
-# FPGA bring-up
-# Make sure to replace with your actual generated bitstream name
 shrike.flash("breakout.bin") 
